fecbb2e55863f94c9b53d21c1c481c48c011f744
Willmar R. de Ramos <willrodrigue2308s@gmail.com>
2025-01-09 19:19:49 -0300

    Projeto inicial

---
+++ site.js
@@ -0,0 +1,9 @@
+function mudarEstilo1() {
+    document.getElementById('estilo').setAttribute('href', 'styles.css');
+}
+function mudarEstilo2() {
+    document.getElementById('estilo').setAttribute('href', 'styles2.css');
+}
+function mudarEstilo3() {
+    document.getElementById('estilo').setAttribute('href', 'styles3.css');
+}
