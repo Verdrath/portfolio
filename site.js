e199908a0f442b4a798adee1afc92da16507849d
Willmar R. de Ramos <willrodrigue2308s@gmail.com>
2025-03-02 20:28:10 -0300

    Adiciona imagens e as muda conforme o estilo

--- site.js
+++ site.js
@@ -1,13 +1,17 @@
 function mudarEstilo1() {
     document.getElementById('estilo').setAttribute('href', 'styles.css');
+    document.getElementById('imagem').setAttribute('src', 'imagens/imagem_estilo1.png');
 }
 function mudarEstilo2() {
     document.getElementById('estilo').setAttribute('href', 'styles2.css');
+    document.getElementById('imagem').setAttribute('src', 'imagens/imagem_estilo2.png');
 }
 function mudarEstilo3() {
     document.getElementById('estilo').setAttribute('href', 'styles3.css');
+    document.getElementById('imagem').setAttribute('src', 'imagens/imagem_estilo3.png');
 }
 
 function tirarEstilo() {
     document.getElementById('estilo').setAttribute('href' , 'style-base.css');
+    document.getElementById('imagem').setAttribute('src', 'imagens/imagem_base.png');
 }
